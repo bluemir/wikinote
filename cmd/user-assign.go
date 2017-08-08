6920a0f4c6342d1c56dfea294b22417efba653de
bluemir <qa22ahj@gmail.com>
2017-08-08 18:36:25 +0900

    Add user assign command

--- cmd/user-assign.go
+++ cmd/user-assign.go
@@ -1,12 +1,45 @@
 package cmd
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/cobra"
+
+	"github.com/bluemir/wikinote/backend"
 )
 
 func NewUserAssignCommand() *cobra.Command {
 	var UserAssignCmd = &cobra.Command{
-		Use: "user COMMAND",
+		Use: "assign USER to ROLE",
+		Args: func(cmd *cobra.Command, args []string) error {
+			// argument validataion
+			if len(args) != 3 {
+				return fmt.Errorf("invaild arguments")
+			}
+			if !strings.EqualFold(args[1], "to") {
+				return fmt.Errorf("invaild arguements")
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			username := args[0]
+			role := args[2]
+
+			b, err := backend.New(backendOpts)
+			if err != nil {
+				return err
+			}
+
+			u, err := b.User().Get(username)
+			if err != nil {
+				return err
+			}
+
+			u.Role = role
+			return b.User().Put(u)
+		},
 	}
+	// vaildate
 	return UserAssignCmd
 }
