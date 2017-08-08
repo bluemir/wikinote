f4a663b17c50d4a895af0b43be6dd34acd9213eb
bluemir <qa22ahj@gmail.com>
2017-08-08 19:06:53 +0900

    Add user delete command

--- cmd/user-remove.go
+++ cmd/user-remove.go
@@ -1,12 +1,32 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/bluemir/wikinote/backend"
 	"github.com/spf13/cobra"
 )
 
 func NewUserRemoveCommand() *cobra.Command {
 	var UserRemoveCmd = &cobra.Command{
-		Use: "rm",
+		Use:     "remove USER",
+		Aliases: []string{"rm", "del", "delete"},
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("Invalid Arguments")
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			username := args[0]
+
+			b, err := backend.New(backendOpts)
+			if err != nil {
+				return err
+			}
+
+			return b.User().Delete(username)
+		},
 	}
 	return UserRemoveCmd
 }
