007a45f84bbd1f571cd82465349e5427f293f5fc
bluemir <qa22ahj@gmail.com>
2017-08-09 15:15:23 +0900

    Implement form save

--- server/update.go
+++ server/update.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
 
@@ -8,6 +9,18 @@ import (
 )
 
 func HandleUpdateForm(c *gin.Context) {
+	p := c.Request.URL.Path
+	data, ok := c.GetPostForm("data")
+	if !ok {
+		c.AbortWithError(http.StatusInternalServerError, fmt.Errorf("error on form"))
+	}
+	err := Backend(c).File().Write(p, []byte(data))
+	if err != nil {
+
+		c.HTML(http.StatusInternalServerError, "/errors/not-found.html", gin.H{})
+		c.Abort()
+	}
+	c.Redirect(http.StatusSeeOther, p)
 }
 func HandleUpdate(c *gin.Context) {
 	p := c.Request.URL.Path
