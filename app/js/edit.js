cf773faca4a78c7dd916391c4e30e69c7a7aa7a1
bluemir <qa22ahj@gmail.com>
2017-09-18 17:08:06 +0900

    Add tab key handler

--- app/js/edit.js
+++ app/js/edit.js
@@ -14,7 +14,20 @@ $.get("article form").on("keydown", function(evt) {
 		if (keyCode == "S") {
 			updateDocument();
 			evt.preventDefault();
+			return
 		}
+	}
+	if (keyCode == "\t") {
+		var $textarea = $.get("textarea[name=data]");
+		var start = $textarea.selectionStart;
+		var end = $textarea.selectionEnd;
+		var data = $textarea.value;
+
+		$textarea.value = data.substring(0, start) + "\t" + data.substring(end);
+		$textarea.selectionStart = $textarea.selectionEnd = start + 1;
+
+		evt.preventDefault();
+		return
 	}
 });
 $.get("body").on("keydown", function(evt) {
